df1e66e6ff30ee0622b1eb822a4d2ba1663b4ba5
Jason R. Coombs <jaraco@jaraco.com>
2015-10-30 09:32:37 -0400

    Allow the dest_ctx to be overridden in the repo_context

--- jaraco/context.py
+++ jaraco/context.py
@@ -192,9 +192,15 @@ def temp_dir(remover=shutil.rmtree):
 
 
 @contextlib.contextmanager
-def repo_context(url, branch=None, quiet=True):
+def repo_context(url, branch=None, quiet=True, dest_ctx=temp_dir):
+	"""
+	Check out the repo indicated by url.
+
+	If dest_ctx is supplied, it should be a context manager
+	to yield the target directory for the check out.
+	"""
 	exe = 'git' if 'git' in url else 'hg'
-	with temp_dir() as repo_dir:
+	with dest_ctx() as repo_dir:
 		cmd = [exe, 'clone', url, repo_dir]
 		if branch:
 			cmd.extend(['--branch', branch])
