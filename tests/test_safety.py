52602fa4dceb8cb0b79f7be98b3c698626eb00bb
Jason R. Coombs <jaraco@jaraco.com>
2026-01-12 21:04:49 -0500

    Update implementation to have different expectations for different cases.

--- tests/test_safety.py
+++ tests/test_safety.py
@@ -1,6 +1,7 @@
 import io
 import tarfile
 import types
+from contextlib import nullcontext as does_not_raise
 
 import pytest
 
@@ -11,51 +12,49 @@ def make_tarball_with(member):
     tar_data = io.BytesIO()
     with tarfile.open(fileobj=tar_data, mode='w') as tar:
         tarinfo = tarfile.TarInfo(name=member.path)
-        tarinfo.size = len(member.content)
-        tar.addfile(tarinfo, io.BytesIO(member.content.encode('ascii')))
+        content = f'content for {member.path}'
+        bin_content = content.encode('ascii')
+        tarinfo.size = len(bin_content)
+        tar.addfile(tarinfo, io.BytesIO(bin_content))
 
     tar_data.seek(0)
     return tar_data
 
 
 cases = [
-    # Legitimate file
     types.SimpleNamespace(
         path='dummy_dir/legitimate_file.txt',
-        content='This file stays in target directory',
-        name='legitimate',
+        expect=does_not_raise(),
     ),
-    # Simple traversal to /tmp
     types.SimpleNamespace(
         path='dummy_dir/../../tmp/pwned_by_zipslip.txt',
-        content='[ZIPSLIP] File written to /tmp via path traversal!',
-        name='pwned_via_tmp',
+        expect=pytest.raises(tarfile.OutsideDestinationError),
     ),
-    # Write to home directory
     types.SimpleNamespace(
         path='dummy_dir/../../../../home/pwned_home.txt',
-        content='[ZIPSLIP] Attempted write to home directory',
-        name='pwned_via_home',
+        expect=pytest.raises(tarfile.OutsideDestinationError),
     ),
-    # current directory parent
     types.SimpleNamespace(
         path='dummy_dir/../escaped.txt',
-        content='[ZIPSLIP] File in parent directory!',
-        name='pwned_escaped',
+        expect=pytest.raises(tarfile.OutsideDestinationError),
     ),
 ]
 
 
 @pytest.fixture(params=cases)
 def tarfile_case(request):
-    return tarfile.open(fileobj=make_tarball_with(request.param), mode='r')
+    with tarfile.open(fileobj=make_tarball_with(request.param), mode='r') as tf:
+        yield types.SimpleNamespace(
+            tarfile=tf,
+            expect=request.param.expect,
+        )
 
 
 def test_zipslip_exploit(tmp_path, tarfile_case):
     """
     Ensure that protections from the default tarfile filter are applied.
     """
-    (member,) = tarfile_case
-    processed_member = jaraco.context.strip_first_component(member, tmp_path)
-    assert '..' not in processed_member.name
-    # tarfile_case.extract(processed_member, path=tmp_path)
+    (member,) = tarfile_case.tarfile
+    with tarfile_case.expect:
+        processed_member = jaraco.context.strip_first_component(member, tmp_path)
+        # tarfile_case.extract(processed_member, path=tmp_path)
